5e1c6de73415a2ae968ca7ed8e1f349a19039688
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-03-24 01:40:52 -0400

    Initial Commit

---
+++ TotallyExpenseTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TotallyExpenseTracker
+//
+//  Created by Kevin Stradtman on 3/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TotallyExpenseTracker/TotallyExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TotallyExpenseTrackerApp.swift
+//  TotallyExpenseTracker
+//
+//  Created by Kevin Stradtman on 3/24/22.
+//
+
+import SwiftUI
+
+@main
+struct TotallyExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
